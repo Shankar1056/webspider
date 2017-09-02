8c9a079c337c1ab544a0863aac7cb2b0bbd80097
Shankar Kumar <shankar@spotsoon.com>
2017-09-02 12:51:27 +0530

    gps

--- app/src/main/java/apextechies/etodo/splash/SplashScreen.java
+++ app/src/main/java/apextechies/etodo/splash/SplashScreen.java
@@ -1,6 +1,8 @@
 package apextechies.etodo.splash;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -8,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.StrictMode;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +18,19 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -28,7 +44,7 @@ import apextechies.etodo.login.LoginActivity;
  * Created by Shankar on 5/11/2017.
  */
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
     Handler h = new Handler();
     int delay = 3000;
     @Override
@@ -47,29 +63,26 @@ public class SplashScreen extends AppCompatActivity {
             window.setStatusBarColor(ContextCompat.getColor(SplashScreen.this, R.color.colorAccent));
         }
         generateHashkey();
-
-        h.postDelayed(new Runnable(){
-            public void run(){
-                if (ClsGeneral.getPreferences(SplashScreen.this, ClsGeneral.USER_ID).equalsIgnoreCase("")) {
-                    startActivity(new Intent(SplashScreen.this, LoginActivity.class));
-                    finish();
+    
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    if (android.os.Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+                        createshortcut();
+                    }
+                
+                    EnableGPSAutoMatically();
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                } finally {
+					
+					
                 }
-              /*  else if (ClsGeneral.getPreferences(SplashScreen.this, ClsGeneral.USER_PHONENUMBER).equalsIgnoreCase("")) {
-                    startActivity(new Intent(SplashScreen.this, PhoneNumberVarification.class));
-                    finish();
-                }*/
-                else {
-
-                        startActivity(new Intent(SplashScreen.this, AllState.class));
-                        finish();
-
-                }
-
-
             }
-        }, delay);
-
-
+        };
+        timer.start();
     }
 
     private void generateHashkey() {
@@ -90,4 +103,143 @@ public class SplashScreen extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+    
+    private void createshortcut() {
+        Intent shortcutIntent = new Intent(getApplicationContext(),
+            SplashScreen.class);
+        
+        shortcutIntent.setAction(Intent.ACTION_MAIN);
+        
+        Intent addIntent = new Intent();
+        addIntent
+            .putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getResources().getString(R.string.app_name));
+        addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+            Intent.ShortcutIconResource.fromContext(getApplicationContext(),
+                R.drawable.logo));
+        
+        addIntent
+            .setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        addIntent.putExtra("duplicate", false);  //may it's already there so don't duplicate
+        getApplicationContext().sendBroadcast(addIntent);
+        
+        
+        
+    }
+    private void EnableGPSAutoMatically() {
+        GoogleApiClient googleApiClient = null;
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).build();
+            googleApiClient.connect();
+            LocationRequest locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(30 * 1000);
+            locationRequest.setFastestInterval(5 * 1000);
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+            
+            // **************************
+            builder.setAlwaysShow(true); // this is the key ingredient
+            // **************************
+            
+            PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi
+                .checkLocationSettings(googleApiClient, builder.build());
+            result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+                @Override
+                public void onResult(LocationSettingsResult result) {
+                    final Status status = result.getStatus();
+                    final LocationSettingsStates state = result
+                        .getLocationSettingsStates();
+                    switch (status.getStatusCode()) {
+                        case LocationSettingsStatusCodes.SUCCESS:
+                            
+                            gotonextactivity();
+                            
+                            break;
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                            toast("GPS is not on");
+                            // Location settings are not satisfied. But could be
+                            // fixed by showing the user
+                            // a dialog.
+                            try {
+                                // Show the dialog by calling
+                                // startResolutionForResult(),
+                                // and check the result in onActivityResult().
+                                status.startResolutionForResult(SplashScreen.this, 1000);
+                                
+                            } catch (IntentSender.SendIntentException e) {
+                                // Ignore the error.
+                            }
+                            break;
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            toast("Setting change not allowed");
+                            // Location settings are not satisfied. However, we have
+                            // no way to fix the
+                            // settings so we won't show the dialog.
+                            break;
+                    }
+                }
+            });
+        }
+        else
+        
+        {
+            
+        }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        
+        if (requestCode == 1000) {
+            if(resultCode == Activity.RESULT_OK){
+                String result=data.getStringExtra("result");
+                gotonextactivity();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                gotonextactivity();
+            }
+        }
+    }
+    
+    private void gotonextactivity() {
+    
+        if (ClsGeneral.getPreferences(SplashScreen.this, ClsGeneral.USER_ID).equalsIgnoreCase("")) {
+            startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+            finish();
+        }
+    
+        else {
+        
+            startActivity(new Intent(SplashScreen.this, AllState.class));
+            finish();
+        
+        }
+        
+    }
+    
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        
+    }
+    
+    @Override
+    public void onConnectionSuspended(int i) {
+        toast("Suspended");
+    }
+    
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        toast("Failed");
+    }
+    private void toast(String message) {
+        try {
+            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+           ex.printStackTrace();
+        }
+    }
+    
 }
